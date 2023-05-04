b3ee8829a22638ff22900580bd305c2423be2c5e
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-04 17:11:44 +0200

    add: simple style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,19 +9,23 @@
 </head>
 <body>
     <h1>Template Blade and Vite</h1>
-    <div>
-        <ul>
+    <div class="container">
+        <div class="row">
             @foreach ($Movies as $Movie)
-                <li>
-                    <h2>{{ $Movie->title }}</h2>
-                    <h3>Originial title: {{ $Movie->original_title }}</h3>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $Movie->title }}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">{{ $Movie->original_title }}</h6>
+                  <div class="card-text">
                     <div>Nationality: {{ $Movie->nationality }}</div>
                     <div>Date: {{$Movie->date}}</div>
                     <div>Rating: {{$Movie->vote}}</div>
+                  </div>
 
-                </li>
+                </div>
+            </div>
             @endforeach
-        </ul>
+        </div>
     </div>
     
 </body>
